69ce5b3ab8384913fb450d6133bc49303dd92fb9
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-02-06 23:04:27 +0100

    update plugin; move voice from build script into custom config file

--- voice.groovy
+++ voice.groovy
@@ -1,5 +1,14 @@
-license {
-    name = 'Arctic License'
-    shortName = 'Arctic'
-    url = 'http://mary.dfki.de/download/arctic-license.html'
+voice {
+    name = 'cmu-rms-hsmm'
+    language = 'en'
+    region = 'US'
+    gender = 'male'
+    type = 'hsmm'
+    description = 'A male US English hidden semi-Markov model voice, built from recordings provided by Carnegie Mellon University'
+    license {
+        name = 'Arctic License'
+        shortName = 'Arctic'
+        url = 'http://mary.dfki.de/download/arctic-license.html'
+    }
+    samplingRate = 16000
 }
